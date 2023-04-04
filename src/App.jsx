eb247d1cdeb0a17d64cb9499eb307cf13d5fc6ea
wumpfroot <markuskojo@msn.com>
2023-04-04 23:45:04 +0100

    Added the frontend mentor link

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ function App() {
 	return (
 		<>
 			<Card />
+			<div class="attribution">
+				Challenge by{" "}
+				<a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+					Frontend Mentor
+				</a>
+				. Coded by{" "}
+				<a href="https://github.com/wumpfroot" target="_blank">
+					Markus Kojo
+				</a>
+				.
+			</div>
 		</>
 	);
 }
